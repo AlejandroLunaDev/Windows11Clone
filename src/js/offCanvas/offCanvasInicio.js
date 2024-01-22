1ad56d0dd6a7ffe242d9c222c9cc4e995f4ebada
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-01-22 10:22:31 -0300

    mejora de ruta relativa a svgs inicio

--- src/js/offCanvas/offCanvasInicio.js
+++ src/js/offCanvas/offCanvasInicio.js
@@ -7,51 +7,51 @@ class Icono {
 
 const iconoGitHub = new Icono(
   "GitHub",
-  "/src/assets/svgs/inicio/github.svg"
+  "./src/assets/svgs/inicio/github.svg"
 );
 const iconoFigma = new Icono(
   "Figma",
-  "/src/assets/svgs/inicio/figma.svg"
+  "./src/assets/svgs/inicio/figma.svg"
 );
 const iconoGitBash = new Icono(
   "Terminal",
-  "/src/assets/svgs/inicio/gitbash.svg"
+  "./src/assets/svgs/inicio/gitbash.svg"
 );
 const iconoChome = new Icono(
   "Chrome",
-  "/src/assets/svgs/inicio/chrome.svg"
+  "./src/assets/svgs/inicio/chrome.svg"
 );
 const iconoExel = new Icono(
   "Exel",
-  "/src/assets/svgs/inicio/exel.svg"
+  "./src/assets/svgs/inicio/exel.svg"
 );
 const iconoWord = new Icono(
   "Word",
-  "/src/assets/svgs/inicio/word.svg"
+  "./src/assets/svgs/inicio/word.svg"
 );
 const iconoVSCode = new Icono(
   "VSCode",
-  "/src/assets/svgs/inicio/vscode.svg"
+  "./src/assets/svgs/inicio/vscode.svg"
 );
 const iconoDiscord = new Icono(
   "Discord",
-  "/src/assets/svgs/inicio/discord.svg"
+  "./src/assets/svgs/inicio/discord.svg"
 );
 const iconolinkedIn = new Icono(
   "LinkedIn",
-  "/src/assets/svgs/inicio/linkedin.svg"
+  "./src/assets/svgs/inicio/linkedin.svg"
 );
 const iconoSteam = new Icono(
   "Steam",
-  "/src/assets/svgs/inicio/steam.svg"
+  "./src/assets/svgs/inicio/steam.svg"
 );
 const iconoWhatsAap = new Icono(
   "WhatsApp",
-  "/src/assets/svgs/inicio/whatsaap.svg"
+  "./src/assets/svgs/inicio/whatsaap.svg"
 );
 const iconoEdge = new Icono(
   "Edge",
-  "/src/assets/svgs/inicio/edge.svg"
+  "./src/assets/svgs/inicio/edge.svg"
 );
 
 
