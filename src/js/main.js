79eb2a943c64304f504df3294efc546ecea49b6b
Alejandro Luna <135073545+AlejandroLunaDev@users.noreply.github.com>
2024-01-22 09:53:47 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { updateClock } from "../js/clock.js";
 import { initializeOffCanvas } from './offCanvas/hook/useOffcanvas.js';
 import { iconosInicio } from "./offCanvas/offCanvasInicio.js";
-import { offCanvasStatus } from "./offCanvas/offcanvasStatus.js";
+import { offCanvasStatus } from "./offCanvas/offCanvasStatus.js";
 
 initializeOffCanvas();
 offCanvasStatus();
