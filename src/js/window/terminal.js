10669ef4594e27ac357b71ad7cd2d5f26eb9427b
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-02-07 10:40:37 -0300

    cambio del contenido de papelera

--- src/js/window/terminal.js
+++ src/js/window/terminal.js
@@ -1,19 +1,1 @@
-/* codigo para abrir y cerrar la terminal */
 
-/* const terminal = () => {
-    const bashIcon = document.querySelector('.bash');
-    const windows = document.getElementById('draggable-window');
-
-
-    const toggleDisplay = () => {
-        windows.style.display = (windows.style.display === 'none') ? 'block' : 'none';
-    };
-
-    const handleClick = () => {
-        toggleDisplay();
-    };
-
-    bashIcon.addEventListener('click', handleClick);
-}
-
-export { terminal }; */
